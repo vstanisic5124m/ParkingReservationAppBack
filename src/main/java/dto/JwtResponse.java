a10dd6728ca8669e2ccc40623916eb425271bdca
vstanisic4619rn <vstanisic4619rn@raf.rs>
2025-12-08 14:01:20 +0100

    dodati data transfer objects, request za mandatory polja, kao i odgovor preko tokena

---
+++ src/main/java/dto/JwtResponse.java
@@ -0,0 +1,23 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private String token;
+
+    @Builder.Default
+    private String type = "Bearer";
+
+    private Long userId;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/dto/RegisterRequest.java
@@ -0,0 +1,33 @@
+package dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name must not exceed 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name must not exceed 50 characters")
+    private String lastName;
+
+    @Size(max = 20, message = "Phone number must not exceed 20 characters")
+    private String phoneNumber;
+}
